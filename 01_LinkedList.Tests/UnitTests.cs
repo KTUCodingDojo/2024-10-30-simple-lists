688025684f7ef1b9968f1471abb9ed088ec71b75
Evaldas Guogis <evaldasguogis@yahoo.com>
2024-10-30 17:58:09 +0200

    Made test testing values method returning nothing

--- 01_LinkedList.Tests/UnitTests.cs
+++ 01_LinkedList.Tests/UnitTests.cs
@@ -1,11 +1,30 @@
-﻿namespace _01_LinkedList.Tests
+﻿
+
+namespace _01_LinkedList.Tests
 {
     public class UnitTests
     {
         [Fact()]
-        public void Test()
+        public void SLinkedList_Values_Test_retuns_empty()
         {
-            true.Should().BeFalse("this test is meant to fail to demonstrate a failing test");
+            SLinkedList list = new SLinkedList();
+
+            //string[] expected = {};
+
+            list.Values().Should().BeEmpty();
         }
+        //[Fact()]
+        //public void SLinkedList_Values_Test_retuns_empty()
+        //{
+        //    SLinkedList list = new SLinkedList();
+
+        //    //list.Add("1");
+        //    //list.Add("2");
+        //    //list.Add("3");
+
+        //    string[] expected = { "1", "2", "3" };
+
+        //    list.Values().Should().Be(expected);
+        //}
     }
 }
---
+++ 01_LinkedList/SLinkedList.cs
@@ -0,0 +1,22 @@
+﻿
+namespace _01_LinkedList
+{
+    public class SLinkedList 
+    {
+        
+        public SLinkedList()
+        {
+        }
+
+        public void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] Values()
+        {
+            
+            return new string[0];
+        }
+    }
+}
